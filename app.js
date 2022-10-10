2830666d5eb8dc5c37d98b020d504278af75157f
Chin Ying Kong <chinYingAdora>
2022-10-11 00:29:36 +1100

    Added session and session store to the app

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const methodOverride = require("method-override")
+const session = require("express-session")
+const mongoDBSession = require("connect-mongodb-session")
 
 const Event = require("./models/events")
 const eventsController = require("./controllers/events")
@@ -15,6 +17,11 @@ const eventsController = require("./controllers/events")
 const app = express()
 const PORT = 3000
 const dbURL = "mongodb://localhost:27017/events"
+const MongoDBStore = mongoDBSession(session)
+const sessionStore = new MongoDBStore({
+  uri: dbURL,
+  collection: 'sessions'
+})
 
 
 // -----------------------------------------------------------
@@ -22,6 +29,14 @@ const dbURL = "mongodb://localhost:27017/events"
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride("_method"))
+app.use(
+    session({
+        secret:"thisisnotgoodsecret",
+        resave: false,
+        saveUninitialized: false,
+        store: sessionStore
+    })
+)
 
 
 // -----------------------------------------------------------
