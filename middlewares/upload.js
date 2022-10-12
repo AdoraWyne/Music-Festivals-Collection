86c690175599001e02fd27ca99c4da31898626c1
Chin Ying Kong <chinYingAdora>
2022-10-12 11:13:02 +1100

    Setup for Cloudinary & installed necessary packages

---
+++ middlewares/upload.js
@@ -0,0 +1,10 @@
+// -----------------------------------------------------------
+// require stuff 
+const multer = require('multer')
+const cloudinary = require('cloudinary').v2
+const { CloudinaryStorage } = require('multer-storage-cloudinary')
+
+require('dotenv').config()
+cloudinary.config()
+
+// -----------------------------------------------------------
